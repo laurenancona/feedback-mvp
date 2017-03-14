a85cd819cd978ab1020996df4306aab718e66412
Lauren Ancona <laurenancona@XCOH2J-6VTFVH7-2.local>
2017-03-14 17:24:29 -0400

    add feedback UI tag

---
+++ tags:feedback-ui.js
@@ -0,0 +1,11 @@
+gaf.init({
+    'open': 'Feedback',
+    'title': 'We would love to hear your thoughts!',
+    'option1': 'Problem',
+    'option2': 'Suggestion',
+    'option3': 'Compliment',
+    'option4': 'Other',
+    'placeholder': 'Please enter your feedback here…',
+    'send': 'Send',
+    'thankyou': 'Thank you for your feedback!'
+});
